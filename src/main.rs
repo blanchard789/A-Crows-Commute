fbbd6bc1952da575801997798d74a12d202eb1eb
Hayden <blanchardhayden45@gmail.com>
2025-02-28 21:07:25 -0800

    Feb 28 1st commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use bevy::{
+    //diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{PresentMode,Window,WindowPlugin,WindowTheme},
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins((
+        DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "A Crow's Commute".into(),
+                name: Some("bevy.app".into()),
+                resolution: (500., 300.).into(),
+                present_mode: PresentMode::AutoVsync,
+                fit_canvas_to_parent: true,
+                prevent_default_event_handling: false,
+                window_theme: Some(WindowTheme::Dark),
+                enabled_buttons: bevy::window::EnabledButtons {
+                    maximize: false,
+                    ..Default::default()
+                },
+                visible: true,
+                ..default()
+            }),
+            ..default()
+        }),
+        //LogDiagnosticsPlugin::default(),
+        //FrameTimeDiagnosticsPlugin::default(),
+    ))
+        .add_systems(Startup, setup)
+        .run();
 }
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Sprite::from_image(
+        asset_server.load("crowGame_downtown.png")),
+        Transform {
+            translation: Vec3::new(0.,0.,0.),
+            scale: Vec3::new(0.70,0.70,1.0),
+            ..Default::default()
+        },
+    ));
+    commands.spawn((
+        Sprite::from_image(asset_server.load("crowGame_crow.png")),
+        Transform {
+            translation: Vec3::new(0.,0.,1.),
+            scale: Vec3::new(1.0,1.0,1.0),
+            ..Default::default()
+        },
+    ));
+}
+
+
