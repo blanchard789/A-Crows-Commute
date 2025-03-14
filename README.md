e9ce46f8eccdaf781e32c1b2ae03cb866abe15ce
Hayden Blanchard <blanchardhayden45@gmail.com>
2025-03-14 16:00:05 -0700

    added some event tracking

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## author: 
 Hayden Blanchard
 ## description:
-I am planning on making a simple game inspired by the way that crows often commute during the day to find food and then group up as a giant flock to keep warm at night. During the winter months I often see crows flying into town in the evening and see them huddling up in trees and on local buildings for the night. During the day though they are either by themselves or in smaller groups throughout the city, so in a way they commute just like humans do although not to a 9-5 job like you or me. My goal for this game is to accomplish a few basic features like the ability to display a 2D crow and to give it the ability to move around a 2D city via inputs from the player. To keep things simple the objectives of this game will just be to find a certain amount of food and to return to the flock before the timer expires signifying the end of the day. There may be smaller objectives like avoiding building along the flight path, but these will be added if I have enough time to achieve all the other goals first.
+A Crow's Commute is a simple 2D game built in Rust using the Bevy crate. The game is inspired by the way that crows will nest together in large groups during winter, often in the urban center of cities. While these crows will spend the night together they are often more seclusive in the day hours, commuting into the city in search for food either by themselves or in smaller groups. When deciding what to do for this project, I thought that this behavior in crows was interesting and something I wanted to base my game off of. The goal of the game is simple, fly the 2D crow over the city map and look for worms, while avoiding the hawk who is following the crow. 
 ## building program:
 Warning: compiling this program may take a few minutes and will use over a GB of storage.   
 cargo run --release
@@ -12,6 +12,10 @@ Project is a WIP
 Forward: W or Up_Arrow  
 Left: A or Left_Arrow  
 Right: D or Right_Arrow  
-Back: S or Down_Arrow  
-
+Back: S or Down_Arrow 
+## testing:
+I am pretty new to Bevy and Rust in general, so my limited knowledge of testing tools and limited time to work on the project meant that I had to do most of my testing in the form of playtesting rather than assertion based testing.  
+I tried my best to do my playtests in a unit testing kind of way, that being that I would create an individual function and then extensively test inputs or events for the expected result in game, before moving on to the next function. 
+## results:
+Overall I am pretty happy with how this project came out. Like I said in the testing section, Bevy and Rust are fairly new environments for me, so building this project was all around a learning experience.
 
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use bevy::{
     prelude::*,
     window::{PresentMode, Window, WindowPlugin, WindowTheme},
 };
-//use rand::Rng;
+use rand::Rng;
 
 // Sets a const global var for the screen size
 const WIDTH: f32 = 1920.;
@@ -38,7 +38,7 @@ fn main() {
         .add_systems(Update, movement)
         .add_systems(Update, collision)
         //.add_systems(Update, despawn_entity)
-        //.add_systems(Update, place_worm)
+        .add_systems(Update, place_worm)
         .add_systems(Update, chase_player)
         .run();
 }
@@ -55,12 +55,20 @@ pub struct WormEntity;
 #[derive(Component)]
 pub struct HawkEntity;
 
-/*
 #[derive(Component)]
 pub struct Active {
     active: bool,
 }
-*/
+
+#[derive(Component)]
+pub struct Count {
+    count: i32,
+}
+
+#[derive(Component)]
+pub struct Status {
+    event_id: i32,
+}
 
 /// ## fn setup()
 /// Initializes spawn settings for game sprites,  
@@ -75,6 +83,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..Default::default()
         },
         BackgroundEntity,
+        Status { event_id: 0 },
     ));
     commands.spawn((
         Sprite::from_image(asset_server.load("crowGame_crow.png")),
@@ -84,6 +93,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..Default::default()
         },
         CrowEntity,
+        Active { active: false },
     ));
     commands.spawn((
         Sprite::from_image(asset_server.load("crowGame_hawk.png")),
@@ -102,14 +112,39 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..Default::default()
         },
         WormEntity,
+        Active { active: false },
+        Count { count: 0 },
     ));
 }
 
 /// ## fn movement()  
 /// Handles player movement key inputs  
-/// and transforms the crow to move in the requested direction  
-fn movement(input: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Transform, With<CrowEntity>>) {
-    for mut transform in query.iter_mut() {
+/// and transforms the crow to move in the requested direction
+//#[allow(clippy::type_complexity)]
+fn movement(
+    input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut Active), With<CrowEntity>>,
+    mut event_query: Query<&mut Status, With<BackgroundEntity>>,
+) {
+    for (mut transform, mut active) in query.iter_mut() {
+        if !active.active
+            && (input.pressed(KeyCode::KeyW)
+                || input.pressed(KeyCode::KeyA)
+                || input.pressed(KeyCode::KeyD)
+                || input.pressed(KeyCode::KeyS)
+                || input.pressed(KeyCode::ArrowUp)
+                || input.pressed(KeyCode::ArrowLeft)
+                || input.pressed(KeyCode::ArrowRight)
+                || input.pressed(KeyCode::ArrowDown))
+        {
+            active.active = true;
+            if let Ok(ref mut stat) = event_query.get_single_mut() {
+                if stat.event_id != 0 {
+                    stat.event_id = 0;
+                }
+            }
+        }
+
         if input.pressed(KeyCode::KeyW) || input.pressed(KeyCode::ArrowUp) {
             transform.rotation = Quat::from_rotation_z(0.0_f32.to_radians());
             transform.translation.y += 1.
@@ -144,17 +179,22 @@ fn movement(input: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Transform, W
 /// Such as the player colliding with worms or hawks.  
 /// If a player collides with a worm the score is increased.  
 /// If a player collides with a hawk the game ends.  
+#[allow(clippy::type_complexity)]
 fn collision(
     crow_query: Query<&Transform, With<CrowEntity>>,
-    worm_query: Query<&Transform, With<WormEntity>>,
+    mut worm_query: Query<
+        (&mut Transform, &mut Active, &mut Count),
+        (With<WormEntity>, Without<CrowEntity>, Without<HawkEntity>),
+    >,
     hawk_query: Query<&Transform, With<HawkEntity>>,
 ) {
     if let Ok(crow_transform) = crow_query.get_single() {
-        if let Ok(worm_transform) = worm_query.get_single() {
-            if (crow_transform.translation.x - worm_transform.translation.x).abs() <= 30.
-                && (crow_transform.translation.y - worm_transform.translation.y).abs() <= 30.
+        if let Ok(ref mut worm_transform) = worm_query.get_single_mut() {
+            if (crow_transform.translation.x - worm_transform.0.translation.x).abs() <= 30.
+                && (crow_transform.translation.y - worm_transform.0.translation.y).abs() <= 30.
             {
-                //println!("Worm");
+                worm_transform.1.active = false;
+                worm_transform.2.count += 1;
             }
         }
         if let Ok(hawk_transform) = hawk_query.get_single() {
@@ -167,76 +207,79 @@ fn collision(
     }
 }
 
-/*
 /// ## fn place_worm()
 /// When the worm is spawned or eaten and a new worm is needed.
 /// This function generates a random number and teleports worm to it.
-fn place_worm(mut query: Query<(&Name, &mut Transform)>) {
-    for (name, mut transform) in query.iter_mut() {
-        if name.as_str() == "Worm" {
+fn place_worm(mut query: Query<(&mut Transform, &mut Active), With<WormEntity>>) {
+    for (mut transform, mut active) in query.iter_mut() {
+        if !active.active {
             let mut rng = rand::rng();
-            let x: i32 = rng.random_range(((-WIDTH + 110.) / 2.) as i32 .. ((WIDTH - 110.) / 2.) as i32);
-            let y: i32 = rng.random_range(((-HEIGHT + 110.) / 2.) as i32 .. ((HEIGHT - 110.) / 2.) as i32);
+            let x: i32 =
+                rng.random_range(((-WIDTH + 110.) / 2.) as i32..((WIDTH - 110.) / 2.) as i32);
+            let y: i32 =
+                rng.random_range(((-HEIGHT + 110.) / 2.) as i32..((HEIGHT - 110.) / 2.) as i32);
             transform.translation.x = x as f32;
             transform.translation.y = y as f32;
+            active.active = true;
         }
     }
 }
-*/
 
 /// ## fn chase_player()
 /// Extracts the player's location (the Crow) and compares it the Hawk's location.  
 /// Then moves Hawk towards the player.  
 fn chase_player(
     mut query: Query<&mut Transform, (With<HawkEntity>, Without<CrowEntity>)>,
-    player_query: Query<&Transform, With<CrowEntity>>,
+    player_query: Query<(&Transform, &Active), With<CrowEntity>>,
 ) {
     if let Ok(c_transform) = player_query.get_single() {
-        for mut transform in query.iter_mut() {
-            if transform.translation.y <= c_transform.translation.y
-                && transform.translation.x == c_transform.translation.x
-            {
-                transform.rotation = Quat::from_rotation_z(0.0_f32.to_radians());
-                transform.translation.y += 1.;
-            } else if transform.translation.x >= c_transform.translation.x
-                && transform.translation.y == c_transform.translation.y
-            {
-                transform.rotation = Quat::from_rotation_z(90.0_f32.to_radians());
-                transform.translation.x -= 1.;
-            } else if transform.translation.x <= c_transform.translation.x
-                && transform.translation.y == c_transform.translation.y
-            {
-                transform.rotation = Quat::from_rotation_z(270.0_f32.to_radians());
-                transform.translation.x += 1.;
-            } else if transform.translation.y >= c_transform.translation.y
-                && transform.translation.x == c_transform.translation.x
-            {
-                transform.rotation = Quat::from_rotation_z(180.0_f32.to_radians());
-                transform.translation.y -= 1.;
-            } else if transform.translation.y <= c_transform.translation.y
-                && transform.translation.x >= c_transform.translation.x
-            {
-                transform.rotation = Quat::from_rotation_z(45.0_f32.to_radians());
-                transform.translation.y += 0.50;
-                transform.translation.x -= 0.50;
-            } else if transform.translation.y < c_transform.translation.y
-                && transform.translation.x < c_transform.translation.x
-            {
-                transform.rotation = Quat::from_rotation_z(315.0_f32.to_radians());
-                transform.translation.y += 0.50;
-                transform.translation.x += 0.50;
-            } else if transform.translation.y > c_transform.translation.y
-                && transform.translation.x > c_transform.translation.x
-            {
-                transform.rotation = Quat::from_rotation_z(135.0_f32.to_radians());
-                transform.translation.y -= 0.50;
-                transform.translation.x -= 0.50;
-            } else if transform.translation.y > c_transform.translation.y
-                && transform.translation.x < c_transform.translation.x
-            {
-                transform.rotation = Quat::from_rotation_z(225.0_f32.to_radians());
-                transform.translation.y -= 0.50;
-                transform.translation.x += 0.50;
+        if c_transform.1.active {
+            for mut transform in query.iter_mut() {
+                if transform.translation.y <= c_transform.0.translation.y
+                    && transform.translation.x == c_transform.0.translation.x
+                {
+                    transform.rotation = Quat::from_rotation_z(0.0_f32.to_radians());
+                    transform.translation.y += 0.5;
+                } else if transform.translation.x >= c_transform.0.translation.x
+                    && transform.translation.y == c_transform.0.translation.y
+                {
+                    transform.rotation = Quat::from_rotation_z(90.0_f32.to_radians());
+                    transform.translation.x -= 0.5;
+                } else if transform.translation.x <= c_transform.0.translation.x
+                    && transform.translation.y == c_transform.0.translation.y
+                {
+                    transform.rotation = Quat::from_rotation_z(270.0_f32.to_radians());
+                    transform.translation.x += 0.5;
+                } else if transform.translation.y >= c_transform.0.translation.y
+                    && transform.translation.x == c_transform.0.translation.x
+                {
+                    transform.rotation = Quat::from_rotation_z(180.0_f32.to_radians());
+                    transform.translation.y -= 0.5;
+                } else if transform.translation.y <= c_transform.0.translation.y
+                    && transform.translation.x >= c_transform.0.translation.x
+                {
+                    transform.rotation = Quat::from_rotation_z(45.0_f32.to_radians());
+                    transform.translation.y += 0.50;
+                    transform.translation.x -= 0.50;
+                } else if transform.translation.y < c_transform.0.translation.y
+                    && transform.translation.x < c_transform.0.translation.x
+                {
+                    transform.rotation = Quat::from_rotation_z(315.0_f32.to_radians());
+                    transform.translation.y += 0.50;
+                    transform.translation.x += 0.50;
+                } else if transform.translation.y > c_transform.0.translation.y
+                    && transform.translation.x > c_transform.0.translation.x
+                {
+                    transform.rotation = Quat::from_rotation_z(135.0_f32.to_radians());
+                    transform.translation.y -= 0.50;
+                    transform.translation.x -= 0.50;
+                } else if transform.translation.y > c_transform.0.translation.y
+                    && transform.translation.x < c_transform.0.translation.x
+                {
+                    transform.rotation = Quat::from_rotation_z(225.0_f32.to_radians());
+                    transform.translation.y -= 0.50;
+                    transform.translation.x += 0.50;
+                }
             }
         }
     }
